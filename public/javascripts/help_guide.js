17b8dfaa88d410651f3cf929da69d9c49ce0a477
plumlike <plumlike7@gmail.com>
2017-03-08 20:45:30 +0900

    release ver 1.0

--- public/javascripts/help_guide.js
+++ public/javascripts/help_guide.js
@@ -11,34 +11,35 @@ $(document).ready(function(){
   		if(e.relatedTarget.id == 'slide2'){
           $('#btn_close2').css('display','none')
           $('#page1,#caption1').css('display','none')
-			    $('#page2,#caption2,#btn_close1,#prev_btn').css('display','block')
           $('#page3,#caption3').css('display','none')
+          $('#page2,#caption2,#btn_close1,#prev_btn').css('display','block')
 			    $('#page4,#caption4').css('display','none')
           $('#page5,#caption5').css('display','none')
         }
         if(e.relatedTarget.id == 'slide3'){
           $('#btn_close2').css('display','none')
-          $('#page1,#caption1').css('display','none')
 			    $('#page2,#caption2').css('display','none')
-        	$('#page3,#caption3,#btn_close1,#prev_btn').css('display','block')
-  	      $('#page4,#caption4').css('display','none')
+          $('#page4,#caption4').css('display','none')
+          $('#page3,#caption3,#btn_close1,#prev_btn').css('display','block')
+          $('#page1,#caption1').css('display','none')
           $('#page5,#caption5').css('display','none')
         }
         if(e.relatedTarget.id == 'slide4'){
         	$('#btn_close2').css('display','none')
-        	$('#page1,#caption1').css('display','none')
-        	$('#page2,#caption2').css('display','none')
 		      $('#page3,#caption3').css('display','none')
+          $('#page5,#caption5').css('display','none')
 		      $('#page4,#caption4,#btn_close1,#prev_btn').css('display','block')
-        	$('#page5,#caption5').css('display','none')
+          $('#page1,#caption1').css('display','none')
+          $('#page2,#caption2').css('display','none')
         }
         if(e.relatedTarget.id == 'slide5'){
+          // $('#btn_close1,#next_btn').css('display','none')
           $('#btn_close1,#next_btn').css('display','none')
-        	$('#page1,#caption1').css('display','none')
-		      $('#page2,#caption2').css('display','none')
-        	$('#page3,#caption3').css('display','none')
 		      $('#page4,#caption4').css('display','none')
 		      $('#page5,#caption5,#btn_close2,#prev_btn').css('display','block')
+          $('#page1,#caption1').css('display','none')
+          $('#page2,#caption2').css('display','none')
+          $('#page3,#caption3').css('display','none')
         }
     })
   })
